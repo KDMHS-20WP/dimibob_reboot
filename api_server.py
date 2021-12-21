05caa8d59f700486bf41d8774ed8e92979cd5bcd
soumt <soumt@dimigo.hs.kr>
2021-12-22 08:47:20 +0900

    api화

---
+++ api_server.py
@@ -0,0 +1,26 @@
+from flask import Flask, make_response
+from flask_restx import Api, Resource
+import datetime
+import os
+import json
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+api = Api(app)
+
+@api.route('/bob')
+class Bob(Resource):
+  def get(self):
+    today = datetime.date.today()
+    today -= datetime.timedelta(days=today.weekday())
+    if(not os.path.exists('datas/'+ today.strftime('%m-%d') + '.json')):
+      exec(open('parser.py', encoding='utf-8').read())
+    with open('datas/'+ today.strftime('%m-%d') + '.json', 'r', encoding='utf-8') as f:
+      result = json.loads(f.read())
+      result = json.dumps(result, ensure_ascii=False, indent=4)
+      res = make_response(result)
+      return res
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000, debug=True)
+    
--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import json
 
 def check_date(date):
   today = datetime.date.today()
   today -= datetime.timedelta(days=today.weekday())
-  
+    
   found_date = datetime.date(1, 1, 1)
 
   for i in range(7):
@@ -22,14 +23,18 @@ bob_articles = soup.select(
   '#dimigo_post_cell_2 > tr'
 )
 
+result_data = {
+  'meals': []
+}
+
 for bob_article in bob_articles:
   #dimigo_post_cell_2 > tr:nth-child(1) > td.title > a
   bob_title = bob_article.find('td', {'class': 'title'}).find('a').text
   bob_link = bob_article.find('td', {'class': 'title'}).find('a').get('href')
+
   if bob_title.find('식단') != -1:
     found_date = check_date(bob_title)
     if(found_date.year != 1):
-      print(found_date.strftime("%m/%d 식단표"))
       bob_req = requests.get(bob_link)
       bob_html = bob_req.text
       bob_soup = BeautifulSoup(bob_html, 'html.parser')
@@ -37,12 +42,21 @@ for bob_article in bob_articles:
       bob_menu = bob_soup.select('#siDoc > ul:nth-child(5) > li > div.scConDoc.clearBar > div')[0]
       bob_menu = bob_menu.text.replace('\n', '')
       
-      josik = bob_menu.partition('*조식 : ')[2].partition('*중식 : ')[0].replace('/', '\n')
-      jungsik = bob_menu.partition('*중식 : ')[2].partition('*석식 : ')[0].replace('/', '\n')
-      soksik = bob_menu.partition('*석식 : ')[2].replace('/', '\n')
+      josik = bob_menu.partition('*조식 : ')[2].partition('*중식 : ')[0].strip() #.replace('/', '\n')
+      jungsik = bob_menu.partition('*중식 : ')[2].partition('*석식 : ')[0].strip() #.replace('/', '\n')
+      soksik = bob_menu.partition('*석식 : ')[2].strip() #.replace('/', '\n')
 
-      print('--조식--\n' + josik + '\n')
-      print('--중식--\n' + jungsik + '\n')
-      print('--석식--\n' + soksik + '\n')
+      
+      data = {
+        'date' : found_date.strftime("%Y-%m-%d"),
+        'josik' : josik,
+        'jungsik' : jungsik,
+        'soksik' : soksik
+      }
+      result_data['meals'].append(data)
 
-      print()
+today = datetime.date.today()
+today -= datetime.timedelta(days=today.weekday())
+with open('datas/'+ today.strftime('%m-%d') + '.json', 'w', encoding='utf-8') as f:
+  res = json.dump(result_data, f, ensure_ascii=False, indent='\t')
+  print('OK')
